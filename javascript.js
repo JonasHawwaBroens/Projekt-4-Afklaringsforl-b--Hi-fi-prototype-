08cb709a98ff435e6e70d3ed78b0ce79a03aad9b
Jonas Hawwa Brøns <70684189+JonasHawwaBroens@users.noreply.github.com>
2021-02-27 18:57:21 +0100

    Kode-opdatering, d. 27/2 2021

--- javascript.js
+++ javascript.js
@@ -5,3 +5,19 @@ function openMenu() {
 function closeMenu() {
     document.getElementById("myMenu").style.height = "0%"
 }
+
+const selected = document.querySelector(".selected");
+const optionsContainer = document.querySelector(".options-container");
+
+const optionsList = document.querySelectorAll(".option");
+
+selected.addEventListener("click", () => {
+    optionsContainer.classList.toggle("active");
+});
+
+optionsList.forEach( o => {
+    o.addEventListener("click", () => {
+        selected.innerHTML = o.querySelector("label").innerHTML;
+        optionsContainer.classList.remove("active");
+    })
+})
